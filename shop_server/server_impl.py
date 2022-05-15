ff5ab1297d84da92c9ef45fd2399316f6b63aa78
pratikpr <prpr4667@colorado.edu>
2022-05-15 00:06:03 -0400

    refactor

--- shop_server/server_impl.py
+++ shop_server/server_impl.py
@@ -1,10 +1,10 @@
 import http
 
 from shop_server import constants
-from shop_server.Item import Item
+from Item import Item
 import json
 # holds the mapping between the name of the item and the object
-from shop_server.status import Status
+from status import Status
 
 items = {}
 # can be used to store the status of the item in the database
